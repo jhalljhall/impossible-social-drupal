eac06cabaf936c95069e4d0abe975f800a918259
jhalljhall <hall.justin@justinhall.com>
2024-01-31 01:30:51 -0500

    adding form handler upon checkout to add subscriber role

--- html/sites/default/settings.php
+++ html/sites/default/settings.php
@@ -264,7 +264,7 @@ $databases = [];
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = 'vYDLg0SWuht5TIorm4Off4wdaees3YbAmu8Rorl1ZSeGx-oBw7rIut0_iT8FoFqGyJ_eZDOTlQ';
+$settings['hash_salt'] = 'FYdqjmdttUfeuEhbxzJbvf0eFdsuzEmEbPaMWQhwKM2CygDkQmJIgUUruf2Cjd2wNsdZ59cWRQ';
 
 /**
  * Deployment identifier.
@@ -830,19 +830,9 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
+
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
 
 $settings['config_sync_directory'] = 'sites/default/files/config_V48VW1KhVpSGILAwIaDnYMh2e5B9sX5lpmj3UBiHKlIav7NypnxGYBfCH6TsO6b1WbJtvaG-lg/sync';
-// $databases['default']['default'] = array (
-//   'database' => 'impossible2222',
-//   'username' => 'root',
-//   'password' => 'root',
-//   'prefix' => '',
-//   'host' => 'localhost',
-//   'port' => '3306',
-//   'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-//   'driver' => 'mysql',
-//   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-// );
