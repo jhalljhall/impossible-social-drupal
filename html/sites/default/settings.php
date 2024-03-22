6c54c6dff208ea2db9d5b4b5eb43ef926ac194f4
jhalljhall <hall.justin@justinhall.com>
2024-03-22 12:49:28 -0400

    algolia installation

--- html/sites/default/settings.php
+++ html/sites/default/settings.php
@@ -830,8 +830,13 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-  include $app_root . '/' . $site_path . '/settings.local.php';
+// if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+//   include $app_root . '/' . $site_path . '/settings.local.php';
+// }
+
+// Automatically generated include for settings managed by ddev.
+if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev.php')) {
+  include __DIR__ . '/settings.ddev.php';
 }
 
 $settings['config_sync_directory'] = 'sites/default/files/config_V48VW1KhVpSGILAwIaDnYMh2e5B9sX5lpmj3UBiHKlIav7NypnxGYBfCH6TsO6b1WbJtvaG-lg/sync';
